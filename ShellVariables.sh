829d637126bbd51191a127db3019c4f70dd3aa28
Anand Chauhan <133531691+anandchauhan108@users.noreply.github.com>
2026-06-14 10:27:45 -0600

    Update ShellVariables.sh

--- ShellVariables.sh
+++ ShellVariables.sh
@@ -16,3 +16,56 @@ Write-Output $getCsvInfo
 #Remove Variable - it removes the variable from memory.
 Remove-Variable -Name getCSvInfo
 write-output $getCsvInfo
+
+# array in Powershell
+$Var=1,2,3
+$var.GetType()
+$var=1
+$var.GetType()
+
+#strongly type array variable
+[Object[]]$arr =1
+$arr.GetType()
+
+#Create Empty array
+$arr1=@()
+$arr1.GetType()
+
+#Create datatype specific array
+[int32[]]$arr2=1,2
+$arr2.GetType()
+
+#multidimentional Array
+$mdarr = @()
+$mdarr += ,@('subject1','Maths','95')
+$mdarr += ,@('subject2','English','93') 
+$mdarr += ,@('subject3','Science','96')
+
+foreach($arr in $mdarr){
+Write-Host $arr #write-host to print in tabular form
+}
+$mdarr[1][1]
+
+#Task1 = Create array and sort it
+$exer = 12,45,24,8,35,17,6,8
+$exer | sort -Descending
+
+#Task2 = Add value to array
+#to modify an array , we have to convert it to arrayList
+$FruitList = 'Apple','Mango','Banana'
+[System.Collections.ArrayList]$fruitArrList= $FruitList
+$fruitArrList.Add('Orange')
+$fruitArrList
+$fruitArrList.Remove('Apple')
+$fruitArrList
+$fruitArrList.Count
+$FruitList.Length
+
+#Task3 = find value in array
+$FruitList.Contains('Orange')
+
+#Task4 = Clear Array
+$FruitList =$null
+$FruitList
+write-host $fruitArrList
+
